db206d12d4a4851dc137b3acabf81ae6e94bddb8
Sumanta Parida <sumantaparida@users.noreply.github.com>
2020-09-18 11:22:54 +0530

    Update config.js
    Changes base url

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // API_BASE: () => `//${window.location.hostname}/api`,
-  // API_BASE: () => 'https://bimapath.herokuapp.com/api',
-  API_BASE: () => 'https://uat.bimapaathshala.org/api',
+  API_BASE: () => 'https://bimapath.herokuapp.com/api',
+  // API_BASE: () => 'https://uat.bimapaathshala.org/api',
   TIMEOUT: 1200000,
 };
